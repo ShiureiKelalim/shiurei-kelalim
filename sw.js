99115337518991746d7a30b6e9b6ccd5ab472da4
ShiureiKelalim <ShiureiKelalim@gmail.com>
2026-07-12 18:18:25 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,45 @@
-const CACHE='sk-v1';
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/index.html'])));
+const CACHE = 'sk-v2';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        '/',
+        '/index.html'
+      ])
+    )
+  );
+
+  self.skipWaiting();
 });
-self.addEventListener('fetch',e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request)).catch(()=>caches.match('/'))
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() =>
+        caches.match('/index.html')
+      )
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request)
+    )
   );
 });
